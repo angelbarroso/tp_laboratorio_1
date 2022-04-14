8c9265c4b216428adf6b7bb7ad88a63250930b80
angelbarroso <angeldavidbarroso1@gmail.com>
2022-04-14 17:09:05 -0300

    primer commit

---
+++ src/TP_1.C
@@ -0,0 +1,8 @@
+/*
+ * TP_1.C
+ *
+ *  Created on: 2 abr. 2022
+ *      Author: Angel Barroso
+ */
+
+
---
+++ src/TP_1_angelBarroso.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : TP_1_angelBarroso.c
+ Author      : angel barroso
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+
+	return EXIT_SUCCESS;
+}
